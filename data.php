5ffb842f366a77adc57ef5248dfc4218309662ec
Pavel Malakovich <molp@tut.by>
2017-03-27 18:38:14 +0300

    hw4_1 second commit

--- data.php
+++ data.php
@@ -3,17 +3,18 @@ $xmlPath = './data.xml';
 $xml = simplexml_load_file($xmlPath);
 
 $attrs = $xml->attributes();
-echo "PurchaseOrder: PurchaseOrderNumber= ".$attrs['PurchaseOrderNumber'] .
-    "   OrderDate= ".$attrs['OrderDate'] . '<br/><br/>';
+echo "PurchaseOrder: PurchaseOrderNumber= " . $attrs['PurchaseOrderNumber'] .
+    "   OrderDate= " . $attrs['OrderDate'] . '<br/><br/>';
+//echo "For Ellen Adams Type of Purchase = " . $xml->Address['Type'] . '<br/>' .
+   // " For Tai Yee Type of Purchase = " . $xml->Address['Type']. '<br/><br/>';
 
-echo "For Ellen Adams Type of Purchase = Shipping".'<br/>' .
-    " For Tai Yee Type of Purchase = Billing" . '<br/><br/>';
-
-echo "For Product Lawnmower PartNumber = 872-AA".'<br/>' .
+echo "For Product Lawnmower PartNumber = 872-AA" . '<br/>' .
     " For Product Lawnmower PartNumber = 926-AA" . '<br/><br/>';
 
-foreach ($xml as $Address) {
-    echo "Name: ".$Address->Name . ' , ' .
+foreach ($xml->Address as $Address) {
+
+    echo "Address Type= " . $xml->Address['Type'] . '<br/>' .
+        "Name: " . $Address->Name . ' , ' .
         "Street: " . $Address->Street . ' , ' .
         "City: " . $Address->City . ' , ' .
         "State: " . $Address->State . ' , ' .
@@ -22,7 +23,7 @@ foreach ($xml as $Address) {
         '<br/><br/>';
 }
 
-echo "DeliveryNotes: ". 'Please leave packages in shed by driveway.' . '<br/><br/>';
+echo "DeliveryNotes: " . 'Please leave packages in shed by driveway.' . '<br/><br/>';
 
 foreach ($xml as $Items) {
     foreach ($Items as $Item) {
